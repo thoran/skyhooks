abf535570f4e5ddd55a528534e58f9cc10e9952c
thoran <code@thoran.com>
2023-02-26 22:05:52 +1100

    Committed automatically by git-importall.

---
+++ lib/File/self.gsubX.rb
@@ -0,0 +1,7 @@
+# File/self.gsubX.rb
+# File.gsub!
+
+# 20180806
+# 0.1.0 (The same version number as the current version of Thoran/File/SelfGsubX.)
+
+require 'Thoran/File/SelfGsubX/self.gsubX'
---
+++ lib/Thoran/File/SelfGsubX/self.gsubX.rb
@@ -0,0 +1,57 @@
+# File/self.gsubX
+# File.gsub!
+
+# 20180806
+# 0.1.0
+
+# Description: This takes a file and replaces any text it finds according to a regular expression with a supplied string.
+
+# Changes since 0.0:
+# 1. + Thoran namespace.
+
+module Thoran
+  module File
+    module SelfGsubX
+
+      def gsub!(filename, replacement_pattern, replacement_text, selection_pattern = nil)
+        replacement_pattern = (
+          case replacement_pattern
+          when String; Regexp.new(Regexp.escape(replacement_pattern))
+          when Regexp; replacement_pattern
+          else; raise
+          end
+        )
+        selection_pattern = replacement_pattern unless selection_pattern
+        selection_pattern = (
+          case selection_pattern
+          when String; Regexp.new(Regexp.escape(selection_pattern))
+          when Regexp; selection_pattern
+          else; raise
+          end
+        )
+        file_handle = self.open(filename, 'r')
+        tmp_file_handle = self.new(filename + '.tmp', 'w')
+        modified = false
+        file_handle.each do |line|
+          if line =~ Regexp.new(selection_pattern)
+            tmp_file_handle.print line.gsub(Regexp.new(replacement_pattern), replacement_text)
+            modified = true
+          else
+            tmp_file_handle.print line
+          end
+        end
+        file_handle.close
+        tmp_file_handle.close
+        if modified
+          self.delete(filename)
+          self.rename(filename + '.tmp', filename)
+        else
+          self.delete(filename + '.tmp')
+        end
+      end
+
+    end
+  end
+end
+
+File.extend(Thoran::File::SelfGsubX)
